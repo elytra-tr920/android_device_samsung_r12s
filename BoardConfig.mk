61d301f8e77c1e84474fb00975ec241cbbadcf8c
borbelyvince <borbely.vince.11@gmail.com>
2025-06-01 00:02:49 +0200

    e1s: Decommonise

---
+++ BoardConfig.mk
@@ -0,0 +1,28 @@
+#
+# Copyright (C) 2014 The Android Open Source Project
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+DEVICE_PATH := device/samsung/e1s
+
+# DTS
+BOARD_DTBO_CFG := $(DEVICE_PATH)/dtbo.cfg
+
+# Display
+TARGET_SCREEN_DENSITY := 418
+
+# Modules
+BOARD_RECOVERY_RAMDISK_KERNEL_MODULES_LOAD := mcd-panel-s6e3fae_e1s.ko sec-auth-ds28e30.ko
+
+# Inherit from common
+include device/samsung/s5e9945/BoardConfigCommon.mk
---
+++ extract-files.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
+
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+namespace_imports = [
+    'vendor/samsung/s5e9945',
+]
+
+
+blob_fixups: blob_fixups_user_type = {
+    'vendor/lib64/hw/audio.primary.s5e9945.so': blob_fixup()
+        .replace_needed('libaudioroute.so', 'libaudioroute_samsung.so')
+        .replace_needed('libtinyalsa.so', 'libtinyalsa_samsung.so'),
+    'vendor/lib64/hw/camera.s5e9945.so': blob_fixup()
+        .add_needed('libui_shim.so'),
+}
+
+module = ExtractUtilsModule(
+    'e1s',
+    'samsung',
+    blob_fixups=blob_fixups,
+    namespace_imports=namespace_imports,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device_with_common(module, 's5e9945', module.vendor)
+    utils.run()
