07c0b9b4821debdf08c5cde2107c838340f56583
𝐈𝐧 𝐮𝐧𝐮𝐦 𝐚𝐝 𝐩𝐮𝐥𝐜𝐡𝐫𝐢𝐨𝐫𝐚 𝐜𝐫𝐚𝐬𝐭𝐢𝐧𝐚~ <94796557+elytra-tr920@users.noreply.github.com>
2025-11-30 19:17:23 +0300

    Update everything

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -22,7 +22,17 @@ BOARD_DTBO_CFG := $(DEVICE_PATH)/dtbo.cfg
 TARGET_SCREEN_DENSITY := 418
 
 # Modules
-BOARD_RECOVERY_RAMDISK_KERNEL_MODULES_LOAD := mcd-panel-s6e3fae_r12s.ko sec-auth-ds28e30.ko
+BOARD_RECOVERY_RAMDISK_KERNEL_MODULES_LOAD := \
+    sec_debug_ssld_info.ko \
+    kiwi_v2.ko \
+    cfg80211.ko
+
+# Copy vendor modules
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/proprietary/vendor/lib/modules/modules.load:vendor/lib/modules/modules.load \
+    $(DEVICE_PATH)/proprietary/vendor/lib/modules/sec_debug_ssld_info.ko:vendor/lib/modules/sec_debug_ssld_info.ko \
+    $(DEVICE_PATH)/proprietary/vendor/lib/modules/kiwi_v2.ko:vendor/lib/modules/kiwi_v2.ko \
+    $(DEVICE_PATH)/proprietary/vendor/lib/modules/cfg80211.ko:vendor/lib/modules/cfg80211.ko
 
 # Inherit from common
 include device/samsung/s5e9945/BoardConfigCommon.mk
